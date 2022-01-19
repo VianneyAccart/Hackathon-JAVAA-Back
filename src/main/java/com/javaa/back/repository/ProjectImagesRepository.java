c852501a086133ccee65d459cd6ea574562b997e
anthony <anthony.charretier@gmail.com>
2022-01-19 17:40:18 +0100

    ready to go

--- src/main/java/com/javaa/back/repository/ProjectImagesRepository.java
+++ src/main/java/com/javaa/back/repository/ProjectImagesRepository.java
@@ -3,9 +3,9 @@ package com.javaa.back.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.javaa.back.entity.User;
+import com.javaa.back.entity.ProjectImages;
 
 @Repository
-public interface UserRepository extends JpaRepository <User, Long> {
+public interface ProjectImagesRepository extends JpaRepository<ProjectImages, Long>{
 
 }
--- src/main/java/com/javaa/back/repository/ProjectRepository.java
+++ src/main/java/com/javaa/back/repository/ProjectRepository.java
@@ -1,9 +1,11 @@
 package com.javaa.back.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.javaa.back.entity.Project;
 
+@Repository
 public interface ProjectRepository extends JpaRepository <Project, Long> {
 
 }
