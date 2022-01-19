1d2f97754b311d1f3ac2b51a18ddd61c1344d5f7
anthony <anthony.charretier@gmail.com>
2022-01-19 18:21:04 +0100

    add entities

--- src/main/java/com/javaa/back/entity/Blog.java
+++ src/main/java/com/javaa/back/entity/Blog.java
@@ -1,5 +1,76 @@
 package com.javaa.back.entity;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+@Entity
 public class Blog {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@JsonIgnore
+	private Long id;
+
+	private String title;
+
+	private String description;
+
+	private String image;
+
+	private String url;
+
+	@ManyToOne(optional = false)
+	@JoinColumn(name = "project_category_id", nullable = false)
+	@JsonIgnore
+	private ProjectCategory projectCategory;
+
+	public Long getId() {
+		return id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getImage() {
+		return image;
+	}
+
+	public void setImage(String image) {
+		this.image = image;
+	}
+
+	public String getUrl() {
+		return url;
+	}
+
+	public void setUrl(String url) {
+		this.url = url;
+	}
+
+	public ProjectCategory getProjectCategory() {
+		return projectCategory;
+	}
+
+	public void setProjectCategory(ProjectCategory projectCategory) {
+		this.projectCategory = projectCategory;
+	}
 
 }
--- src/main/java/com/javaa/back/entity/Professional.java
+++ src/main/java/com/javaa/back/entity/Professional.java
@@ -1,5 +1,66 @@
 package com.javaa.back.entity;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+@Entity
 public class Professional {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@JsonIgnore
+	private Long id;
+
+	private String name;
+
+	private String image;
+
+	private String description;
+
+	@ManyToOne(optional = false)
+	@JoinColumn(name = "activity_id", nullable = false)
+	@JsonIgnore
+	private Activity activity;
+
+	public Long getId() {
+		return id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getImage() {
+		return image;
+	}
+
+	public void setImage(String image) {
+		this.image = image;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public Activity getActivity() {
+		return activity;
+	}
+
+	public void setActivity(Activity activity) {
+		this.activity = activity;
+	}
 
 }
