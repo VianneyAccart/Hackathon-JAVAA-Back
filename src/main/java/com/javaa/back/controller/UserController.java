58415e7710405e3128eb03f585e5ccb512967db8
anthony <anthony.charretier@gmail.com>
2022-01-20 10:20:59 +0100

    add get one user

--- src/main/java/com/javaa/back/controller/UserController.java
+++ src/main/java/com/javaa/back/controller/UserController.java
@@ -5,6 +5,7 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,6 +22,11 @@ public class UserController {
 	
 	@Autowired
 	UserService userService;
+	
+	@GetMapping("/{id}")
+	public User findOne(@PathVariable Long id) {
+		return userService.getOne(id);
+	}
 	
 	//create user
 	@PostMapping
