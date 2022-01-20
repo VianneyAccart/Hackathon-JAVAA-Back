77165f849f2deb948811e0114c9c28061d2c58dc
AdN-WeB <adrien.mervelay84@gmail.com>
2022-01-20 14:27:43 +0100

    resolved bug

--- src/main/java/com/javaa/back/service/ProductProjectCategoryService.java
+++ src/main/java/com/javaa/back/service/ProductProjectCategoryService.java
@@ -1,15 +1,10 @@
 package com.javaa.back.service;
 
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 
 import org.springframework.stereotype.Service;
 
-
-import com.javaa.back.dto.ProductProjectCategoryDto;
-import com.javaa.back.entity.Product;
 import com.javaa.back.repository.ProductProjectCategoryRepository;
 
 
